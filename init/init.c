d70a8ec14460f274d53089eabad6c5c1f16e3415
Colton Acosta <cacost12@asu.edu>
2023-03-07 12:22:13 -0700

    set pwm initialization for 1ms pulses

--- init/init.c
+++ init/init.c
@@ -126,7 +126,7 @@ TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 htim15.Instance               = TIM15;
 htim15.Init.Prescaler         = 0;
 htim15.Init.CounterMode       = TIM_COUNTERMODE_UP;
-htim15.Init.Period            = 65535;
+htim15.Init.Period            = 63999;
 htim15.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;
 htim15.Init.RepetitionCounter = 0;
 htim15.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -158,7 +158,7 @@ if ( HAL_TIMEx_MasterConfigSynchronization( &htim15, &sMasterConfig ) != HAL_OK 
 
 /* Setup PWM Outputs */
 sConfigOC.OCMode       = TIM_OCMODE_PWM1;
-sConfigOC.Pulse        = 0;
+sConfigOC.Pulse        = 32000;
 sConfigOC.OCPolarity   = TIM_OCPOLARITY_HIGH;
 sConfigOC.OCNPolarity  = TIM_OCNPOLARITY_HIGH;
 sConfigOC.OCFastMode   = TIM_OCFAST_DISABLE;
